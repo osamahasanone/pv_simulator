0396cd8484957c02dcf0b9c202732d04a5ce23e7
Osama <osamahasanone@gmail.com>
2021-07-12 10:40:33 +0300

    bugfix w to kw

--- pv_simulator.py
+++ pv_simulator.py
@@ -31,14 +31,15 @@ class PVSimulator(RabbitMQConsumer):
         self.csv_helper = CSVHelper(path=csv_path)
 
     def current_pv_value(self):
-        return random.randint(0, 9)
+        return random.randint(0, 9000)
 
     def callback(self, ch, method, properties, body):
         '''To be called when a message detected in the queue'''
-        received_watts = int(body)/1000
-        simulated_pv = self.current_pv_value()
+        received_watts = int(body)
+        received_kw = round(received_watts/1000, 2)
+        simulated_pv_kw = round(self.current_pv_value()/1000, 2)
         self.csv_helper.append_line(
-            [datetime.now().strftime('%Y-%m-%d %H:%M:%S'), received_watts, simulated_pv, received_watts+simulated_pv])
+            [datetime.now().strftime('%Y-%m-%d %H:%M:%S'), received_kw, simulated_pv_kw, round(received_kw+simulated_pv_kw, 2)])
         print(f' {received_watts} watts dequeued from {self.queue} queue')
 
     def start(self):
